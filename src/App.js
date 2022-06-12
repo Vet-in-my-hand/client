4f48e0d2256d75969ddbd3df518b93d4ea8f4cbd
Velo248 <japark4884@gmail.com>
2022-06-12 09:02:49 +0900

    라인정리

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import UserMain from './components/user/userMain';
 import HospitalMain from './components/hospital/hospitalMain';
 import ReserveList from './components/reserve/reserveList';
 import CareList from './components/care/careList';
+import MainCover from './components/cover/mainCover';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='hospital/register' element={<HospitalRegister />}></Route>
-          <Route path='user/register' element={<UserRegister />}></Route>
+          <Route path='/' element={<MainCover />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
-          <Route path='/user/main' element={<UserMain />}></Route>
+          <Route path='/hospital/register' element={<HospitalRegister />}></Route>
           <Route path='/hospital/main' element={<HospitalMain />}></Route>
           <Route path='/hospital/reserve' element={<ReserveList />}></Route>
           <Route path='/hospital/care' element={<CareList />}></Route>
+          <Route path='/user/register' element={<UserRegister />}></Route>
+          <Route path='/user/main' element={<UserMain />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
